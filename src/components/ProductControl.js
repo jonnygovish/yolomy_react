58f26a06c5696bb9b82e96adfa980e9bbd830da4
john mutavi <jonnygovish@gmail.com>
2020-08-04 11:58:43 +0300

    Create a  ProductControl component

---
+++ src/components/ProductControl.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ProductList from './ProductList';
+
+class ProductControl extends Component {
+    constructor(props){
+        super(props);
+        this.state ={}
+    }
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        )
+    }
+}
+
+export default ProductControl
