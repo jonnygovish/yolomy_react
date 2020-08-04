44f64ccd66eaa69f12f416d4cc7a163717bbe06d
john mutavi <jonnygovish@gmail.com>
2020-08-04 12:04:10 +0300

    Remove actualProductList from ProductList component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,56 +4,9 @@ import '../styles/productList.css';
 
 import Product from './Product';
 
-import tshirt from '../images/products/tshirt.png';
-import backpack from '../images/products/backpack.png';
-import pants from '../images/products/pants.png';
-import trekkingshoes from '../images/products/trekkingshoes.png';
-import giacket from '../images/products/giacket.png';
-import tshirt_ladies from '../images/products/tshirt_ladies.png';
 
-// ActualProductList
-const actualProductList = [
-    
-   
-    {
-        name: 'T-Shirt',
-        price: '599',
-        photo: tshirt,
-        id: "1"
-    },
-    {
-        name: 'BackPack', 
-        price: '1500',
-        photo: backpack,
-        id: "2"
-    },
-    {
-        name: 'Pants', 
-        price: '1000',
-        photo: pants,
-        id: '3'
-    },
-    {
-        name: 'Trekking Shoes',
-        price: '2000',
-        photo: trekkingshoes,
-        id: '4'
-    },
-    {
-        name: 'Jacket',
-        price: '1500',
-        photo: giacket,
-        id: '5'
-    },
-    {
-        name:'T-Shirt Ladies',
-        price: '650',
-        photo: tshirt_ladies,
-        id: '6'
-    }
-]
 
-function ProductList() {
+function ProductList(props) {
     return (
         <React.Fragment>
         <div className='container' id="products">
@@ -74,7 +27,7 @@ function ProductList() {
             {/* Product List */}
             <div className="men-products">
                 <div className="row">
-                {actualProductList.map((product) =>
+                {props.productList.map((product) =>     //Loop through the product list passed down from ProductControl component
                     <  Product  
                     photo = {product.photo}
                     name = {product.name}
