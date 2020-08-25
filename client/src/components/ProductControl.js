dc12b69f23934ed34ded7589aa2f36f518ac6fb9
john mutavi <jonnygovish@gmail.com>
2020-08-25 06:45:26 +0300

    Fetching and displaying product from the database

--- client/src/components/ProductControl.js
+++ client/src/components/ProductControl.js
@@ -1,58 +1,12 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+//component imports
 import AddProduct from './AddProduct';
 import ProductList from './ProductList';
 import NewProductForm from './NewProductForm';
-
-import tshirt from '../images/products/tshirt.png';
-import backpack from '../images/products/backpack.png';
-import pants from '../images/products/pants.png';
-import trekkingshoes from '../images/products/trekkingshoes.png';
-import giacket from '../images/products/giacket.png';
-import tshirt_ladies from '../images/products/tshirt_ladies.png';
-import Default_image from '../images/product_image.jpeg';
 import ProductDetail from './ProductDetail';
 
-// ActualProductList
-const actualProductList = [
-    
-   
-    {
-        name: 'T-Shirt',
-        price: '599',
-        photo: tshirt,
-        id: "1"
-    },
-    {
-        name: 'BackPack', 
-        price: '1500',
-        photo: backpack,
-        id: "2"
-    },
-    {
-        name: 'Pants', 
-        price: '1000',
-        photo: pants,
-        id: '3'
-    },
-    {
-        name: 'Trekking Shoes',
-        price: '2000',
-        photo: trekkingshoes,
-        id: '4'
-    },
-    {
-        name: 'Jacket',
-        price: '1500',
-        photo: giacket,
-        id: '5'
-    },
-    {
-        name:'T-Shirt Ladies',
-        price: '650',
-        photo: tshirt_ladies,
-        id: '6'
-    }
-]
 
 
 
@@ -61,9 +15,18 @@ class ProductControl extends Component {
         super(props);
         this.state ={
             productFormVisible: false,
-            actualProductList: actualProductList, 
+            actualProductList: [], 
             selectedProduct: null
         }
+    }
+    componentDidMount(){
+        axios.get('http://localhost:5000/products')
+        .then(res =>{
+            console.log(res)
+            this.setState({
+                actualProductList: res.data
+            })
+        })
     }
 
     handleClick = ()=>{
@@ -83,15 +46,30 @@ class ProductControl extends Component {
 
     // Method to handle adding a new product
     handleAddingNewProduct = (newProduct) =>{
-        if (newProduct.photo === undefined){
-            newProduct.photo = Default_image
-        }
-        const newProductList = this.state.actualProductList.concat(newProduct)
+        // if (newProduct.photo === undefined){
+        //     newProduct.photo = Default_image
+        // }
+        // const newProductList = this.state.actualProductList.concat(newProduct)
+
+        axios.post('http://localhost:5000/products', newProduct)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err))
         this.setState({
-            actualProductList: newProductList,
             productFormVisible: false
         })
     };
+    handleDeletingProduct = (id) =>{
+        axios.delete('http://localhost:5000/api/products/'+id)
+            .then(res => console.log(res.data))
+            .catch((error) =>{
+                console.log(error)
+            })
+            this.setState({
+                actualProductList: this.state.actualProductList.filter(product => product._id !== id),
+                formVisibleOnPage: false,
+                selectedProduct: null
+            })
+    }
 
      // Method to handle click event on a product
      handleChangingSelectedProduct = (id) => {
@@ -102,7 +80,7 @@ class ProductControl extends Component {
         let currentVisibleState = null;
         let buttonText = null
         if(this.state.selectedProduct != null){
-            currentVisibleState = <ProductDetail  product ={this.state.selectedProduct} /> //new code
+            currentVisibleState = <ProductDetail  product ={this.state.selectedProduct} onDeleteProduct = {this.handleDeletingProduct}/> //new code
             buttonText = 'Back to Product List '
         } else if (this.state.productFormVisible){
             currentVisibleState = <NewProductForm  onNewProductCreation= {this.handleAddingNewProduct}/>
